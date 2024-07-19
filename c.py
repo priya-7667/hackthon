8e8bee04f51f2dfea0b9d9bc56fdff879dbb0c07
priya-7667 <134557197+priya-7667@users.noreply.github.com>
2024-07-19 17:59:44 +0200

    Update c.py

--- c.py
+++ c.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly as px
+import plotly.express as px
 from sklearn.linear_model import LinearRegression
 from datetime import datetime
 
